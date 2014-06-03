lightsofapollo/gaia

Bug 998362 - First integration test using marionette js

apps/camera/test/marionette/capture_test.js
@@ -1,0 +1,36 @@
+marionette('Capture', function() {
+  'use strict';
+
+  var assert = require('assert');
+  var client = marionette.client();
+  var $ = require('./lib/mquery')(client)
+  var camera = new (require('./lib/camera'))(client);
+
+  setup(function() {
+    camera.restart();
+  });
+
+  test('capture a picture', function() {
+    $('.test-capture').tap();
+    client.helper.waitForElement('.test-thumbnail');
+  });
+
+  test('capture a video', function(done) {
+    $('.test-switch').tap();
+    camera.waitForPreviewReady();
+    $('.test-capture').tap();
+    client.helper.waitForElement('.recording-timer.visible');
+    // It records a 3 seconds video
+    setTimeout(function(){
+      $('.test-capture').tap();
+      client.helper.waitForElement('.test-thumbnail');
+      done();
+    }, 3000);
+  });
+
+  test('capture a picture', function() {
+    $('.test-capture').tap();
+    client.helper.waitForElement('.test-thumbnail');
+  });
+
+});

a-os/camera

test/marionette/capture_test.js
@@ -1,0 +1,34 @@
+marionette('Capture', function() {
+  'use strict';
+
+  teardown(function() {
+    camera.close();
+  });
+
+  var assert = require('assert');
+  var client = marionette.client();
+  var camera = new (require('./lib/camera'))(client);
+
+  setup(function() {
+    camera.launch();
+  });
+
+  test('capture a picture', function() {
+    camera.tapCapture();
+    camera.waitForThumbnail();
+  });
+
+  test('capture a video', function(done) {
+    camera.tapModeSwitch();
+    camera.waitForPreviewReady();
+    camera.tapCapture();
+    camera.waitForRecordingTimer();
+    // It records a 3 seconds video
+    setTimeout(function(){
+      camera.tapCapture();
+      camera.waitForThumbnail();
+      done();
+    }, 3000);
+  });
+
+});