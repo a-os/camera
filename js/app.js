mozilla-b2g/gaia

Bug 1049259 - [B2G][Notes] Cannot attach a picture directly from the camera when creating a new note

apps/camera/js/app.js
@@ -188,7 +188,7 @@ App.prototype.bindEvents = function() {
   this.once('viewfinder:visible', this.onCriticalPathDone);
   this.once('storage:checked:healthy', this.geolocationWatch);
   this.on('busy', this.onBusy);
-  this.on('ready', this.onReady);
+  this.on('ready', this.clearLoading);
   this.on('visible', this.onVisible);
   this.on('hidden', this.onHidden);
 
@@ -388,6 +388,21 @@ App.prototype.showLoading = function(delay) {
 };
 
 /**
+ * Clears the loadings screen, or
+ * any pending loading screen.
+ *
+ * @private
+ */
+App.prototype.clearLoading = function() {
+  debug('clear loading');
+  var view = this.views.loading;
+  clearTimeout(this.loadingTimeout);
+  if (!view) { return; }
+  view.hide(view.destroy);
+  this.views.loading = null;
+};
+
+/**
  * When the camera indicates it's busy it
  * sometimes passes a `type` string. When
  * this type matches one of our keys in the
@@ -401,21 +416,6 @@ App.prototype.showLoading = function(delay) {
 App.prototype.onBusy = function(type) {
   var delay = this.settings.loadingScreen.get(type);
   if (delay) { this.showLoading(delay); }
-};
-
-/**
- * Clears the loadings screen, or
- * any pending loading screen.
- *
- * @private
- */
-App.prototype.onReady = function() {
-  debug('clear loading');
-  var view = this.views.loading;
-  clearTimeout(this.loadingTimeout);
-  if (!view) { return; }
-  view.hide(view.destroy);
-  this.views.loading = null;
 };
 
 });

apps/camera/js/controllers/activity.js
@@ -248,11 +248,16 @@ ActivityController.prototype.onActivityCanceled = function() {
 
 // TODO: Messy, tidy up!
 ActivityController.prototype.onActivityConfirmed = function(newMedia) {
+  var self = this;
   var activity = this.activity;
   var needsResizing;
   var media = {
     blob: newMedia.blob
   };
+
+  // In low end devices resizing can be slow.
+  // We display a spinner
+  this.app.showLoading();
 
   // Video
   if (newMedia.isVideo) {
@@ -271,15 +276,20 @@ ActivityController.prototype.onActivityConfirmed = function(newMedia) {
         blob: newMedia.blob,
         width: activity.source.data.width,
         height: activity.source.data.height
-      }, function(resizedBlob) {
-        media.blob = resizedBlob;
-        activity.postResult(media);
-      });
+      }, onImageResized);
       return;
     }
   }
 
-  this.activity.postResult(media);
+  function onImageResized(resizedBlob) {
+    media.blob = resizedBlob;
+    activity.postResult(media);
+    self.app.clearLoading();
+  }
+
+  activity.postResult(media);
+  this.app.clearLoading();
+
 };
 
 });

apps/camera/test/unit/app_test.js
@@ -122,7 +122,7 @@ suite('app', function() {
     // methods before any of this happens, so that the
     // spy is always at the root of any call.
     this.sandbox.spy(this.App.prototype, 'boot');
-    this.sandbox.spy(this.App.prototype, 'onReady');
+    this.sandbox.spy(this.App.prototype, 'clearLoading');
 
     // Aliases
     this.settings = options.settings;
@@ -226,9 +226,9 @@ suite('app', function() {
       var callback = on.args[0][1];
 
       // Call the callback and make sure
-      // that `onReady` was called.
+      // that `clearLoading` was called.
       callback();
-      sinon.assert.calledOnce(this.App.prototype.onReady);
+      sinon.assert.calledOnce(this.App.prototype.clearLoading);
     });
 
     suite('App#geolocationWatch()', function() {
@@ -262,7 +262,7 @@ suite('app', function() {
         this.spy = this.app.once.withArgs('viewfinder:visible');
         this.callback = this.spy.args[0][1];
 
-        sinon.stub(this.app, 'onReady');
+        sinon.stub(this.app, 'clearLoading');
         sinon.spy(this.app, 'loadController');
         sinon.spy(this.app, 'loadL10n');
 
@@ -417,7 +417,7 @@ suite('app', function() {
     });
   });
 
-  suite('App#onReady()', function() {
+  suite('App#clearLoading()', function() {
     setup(function() {
       this.sandbox.spy(window, 'clearTimeout');
       this.sandbox.stub(window, 'setTimeout').returns('<timeout-id>');
@@ -427,20 +427,20 @@ suite('app', function() {
 
     test('Should clear loadingTimeout', function() {
       this.app.showLoading();
-      this.app.onReady();
+      this.app.clearLoading();
       sinon.assert.calledWith(window.clearTimeout, '<timeout-id>');
     });
 
     test('Should hide, then destroy the view', function() {
       var view = this.app.views.loading;
-      this.app.onReady();
+      this.app.clearLoading();
 
       sinon.assert.called(view.hide);
       assert.ok(view.destroy.calledAfter(view.hide));
     });
 
     test('Should clear reference to `app.views.loading`', function() {
-      this.app.onReady();
+      this.app.clearLoading();
       assert.equal(this.app.views.loading, null);
     });
   });

apps/camera/test/unit/controllers/camera_test.js
@@ -479,7 +479,7 @@ suite('controllers/camera', function() {
     test('It clears loading after the camera has loaded', function() {
       this.controller.onGalleryClosed();
       this.camera.load.args[0][0]();
-      sinon.assert.called(this.app.onReady);
+      sinon.assert.called(this.app.clearLoading);
     });
 
     test('It doesn\'t load the camera if the app is hidden', function() {

a-os/camera

js/app.js
@@ -163,7 +163,7 @@ App.prototype.bindEvents = function() {
   this.once('viewfinder:visible', this.onCriticalPathDone);
   this.once('storage:checked:healthy', this.geolocationWatch);
   this.on('busy', this.onBusy);
-  this.on('ready', this.onReady);
+  this.on('ready', this.clearSpinner);
   this.on('visible', this.onVisible);
   this.on('hidden', this.onHidden);
 
@@ -389,6 +389,21 @@ App.prototype.showSpinner = function(key) {
     self.views.loading.appendTo(self.el).show();
     debug('loading shown');
   }, ms);
+};
+
+/**
+ * Clears the loadings screen, or
+ * any pending loading screen.
+ *
+ * @private
+ */
+App.prototype.clearSpinner = function() {
+  debug('clear loading');
+  var view = this.views.loading;
+  clearTimeout(this.spinnerTimeout);
+  if (!view) { return; }
+  view.hide(view.destroy);
+  this.views.loading = null;
 };
 
 /**

js/controllers/activity.js
@@ -261,11 +261,16 @@ ActivityController.prototype.onActivityCanceled = function() {
 
 // TODO: Messy, tidy up!
 ActivityController.prototype.onActivityConfirmed = function(newMedia) {
+  var self = this;
   var activity = this.activity;
   var needsResizing;
   var media = {
     blob: newMedia.blob
   };
+
+  // In low end devices resizing can be slow.
+  // We display a spinner
+  this.app.showSpinner();
 
   // Video
   if (newMedia.isVideo) {
@@ -284,15 +289,20 @@ ActivityController.prototype.onActivityConfirmed = function(newMedia) {
         blob: newMedia.blob,
         width: activity.source.data.width,
         height: activity.source.data.height
-      }, function(resizedBlob) {
-        media.blob = resizedBlob;
-        activity.postResult(media);
-      });
+      }, onImageResized);
       return;
     }
   }
 
-  this.activity.postResult(media);
+  function onImageResized(resizedBlob) {
+    media.blob = resizedBlob;
+    activity.postResult(media);
+    self.app.clearSpinner();
+  }
+
+  activity.postResult(media);
+  this.app.clearSpinner();
+
 };
 
 });

test/unit/app_test.js
@@ -116,6 +116,7 @@ suite('app', function() {
     this.sandbox.spy(this.App.prototype, 'boot');
     this.sandbox.spy(this.App.prototype, 'onReady');
     this.sandbox.spy(this.App.prototype, 'showSpinner');
+    this.sandbox.spy(this.App.prototype, 'clearSpinner');
 
     // Aliases
     this.settings = options.settings;
@@ -212,9 +213,9 @@ suite('app', function() {
       var callback = on.args[0][1];
 
       // Call the callback and make sure
-      // that `onReady` was called.
+      // that `clearSpinner` was called.
       callback();
-      sinon.assert.calledOnce(this.App.prototype.onReady);
+      sinon.assert.calledOnce(this.App.prototype.clearSpinner);
     });
 
     test('It calls `showSpinner`', function() {
@@ -270,6 +271,7 @@ suite('app', function() {
 
         sinon.stub(this.app, 'onReady');
         sinon.spy(this.app, 'loadLazyControllers');
+        sinon.stub(this.app, 'clearSpinner');
         sinon.spy(this.app, 'loadL10n');
 
         // Call the callback to test
@@ -426,7 +428,7 @@ suite('app', function() {
     });
   });
 
-  suite('App#onReady()', function() {
+  suite('App#clearSpinner()', function() {
     setup(function() {
       this.sandbox.spy(window, 'clearTimeout');
       this.sandbox.stub(window, 'setTimeout').returns('<timeout-id>');
@@ -437,19 +439,20 @@ suite('app', function() {
     test('Should clear loadingTimeout', function() {
       this.app.showSpinner();
       this.app.onReady();
+      this.app.clearSpinner();
       sinon.assert.calledWith(window.clearTimeout, '<timeout-id>');
     });
 
     test('Should hide, then destroy the view', function() {
       var view = this.app.views.loading;
-      this.app.onReady();
+      this.app.clearSpinner();
 
       sinon.assert.called(view.hide);
       assert.ok(view.destroy.calledAfter(view.hide));
     });
 
     test('Should clear reference to `app.views.loading`', function() {
-      this.app.onReady();
+      this.app.clearSpinner();
       assert.equal(this.app.views.loading, null);
     });
   });

test/unit/controllers/camera_test.js
@@ -488,7 +488,7 @@ suite('controllers/camera', function() {
     test('It clears loading after the camera has loaded', function() {
       this.controller.onGalleryClosed();
       this.camera.load.args[0][0]();
-      sinon.assert.called(this.app.onReady);
+      sinon.assert.called(this.app.clearSpinner);
     });
 
     test('It doesn\'t load the camera if the app is hidden', function() {